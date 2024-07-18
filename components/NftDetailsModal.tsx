a012a895ac406c0d84c8fc78818276879099a65e
Marko Cen <xiaoyu.cen@circle.com>
2024-07-18 15:40:20 -0700

    fix: details modal styling

--- components/NftDetailsModal.tsx
+++ components/NftDetailsModal.tsx
@@ -30,21 +30,21 @@ export const NftDetailsModal: React.FC<Props> = ({ open, onClose, token }) => {
             transition
             className="min-h-full relative transform overflow-hidden rounded-lg bg-[#29233b] px-4 pb-6 pt-5 text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 sm:w-full sm:max-w-3xl sm:p-6 data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95"
           >
-            <div className="flex gap-5 flex-col md:flex-row">
-              <div className='basis-3/4'>
+            <div className="flex gap-8 flex-col md:flex-row">
+              <div className='shrink-0'>
                 <Image
                   className='mx-auto'
                   src={`/nft-pics/${token.tokenId}.png`} width={300} height={300} alt={token.title ?? ''}/>
               </div>
-              <div className="mt-2 flex flex-col gap-6">
-                <DialogTitle as="h3" className="text-4xl leading-6">{token.title}</DialogTitle>
+              <div className="mt-2 mb-6 flex flex-col gap-6">
+                <DialogTitle as="h3" className="text-4xl leading-10 break-words">{token.title}</DialogTitle>
                 <div className='flex flex-col gap-4'>
-                  <p className="text-gray-400 text-2xl">
+                  <p className="text-gray-400 text-2xl break-words">
                     {token.description}
                   </p>
                   <p className="text-gray-400 break-words text-2xl">
                     NFT Contract: <Link className='underline' href={`https://polygonscan.com/address/${token.address}`} target='_blank'
-                                   rel="noopener noreferrer">{token.address}</Link>
+                                   rel="noopener noreferrer">{token.address.substring(0, 6)}...{token.address.substring(36)}</Link>
                   </p>
                 </div>
               </div>
